4d4fc92b0dcdd80c2f31119141ef339c965a00ac
Ricardo Vinciguerra <ricardorhv.dev@gmail.com>
2025-03-19 21:51:57 -0300

    feat: validation to verifiy if exists a driver with document and email

--- src/driver/driver.service.ts
+++ src/driver/driver.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { BadRequestException, Injectable, NotFoundException } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { CreateDriverDto } from './dto/create-driver.dto';
 import { UpdateDriverDto } from './dto/update-driver.dto';
@@ -18,24 +18,52 @@ export class DriverService {
       phone: driver.phone,
       isActive: driver.isActive,
       email: driver.email,
-      password: driver.password
+      password: driver.password,
+      createdAt: driver.createdAt,
     }
   }
 
-  create(createDriverDto: CreateDriverDto) {
-    const driver = this.prisma.driver.create({
+  async create(createDriverDto: CreateDriverDto) {
+    const driverAlreadyExists = await this.prisma.driver.findFirst({
+      select: {
+        document: true,
+        email: true
+      },
+      where: {
+        OR: [
+          {
+            document: createDriverDto.document
+          },
+          {
+            email: createDriverDto.email
+          }
+        ]
+      }
+    })
+
+    if (driverAlreadyExists) {
+      
+      if (driverAlreadyExists.email === createDriverDto.email){
+        throw new BadRequestException(`Already exists a driver with email ${createDriverDto.email}`)
+      }
+
+      if (driverAlreadyExists.document === createDriverDto.document){
+        throw new BadRequestException(`Already exists a driver with document ${createDriverDto.document}`)
+      }
+    }
+
+    const driver = await this.prisma.driver.create({
       data: {
         ...createDriverDto,
         isActive: true
       }
     })
-
     return driver;
   }
 
   async findAll(): Promise<Driver[]> {
-    const drivers = await this.prisma.driver.findMany();
-    return drivers.map((driver) => this.mapToEntity(driver));
+    const drivers = await this.prisma.driver.findMany()
+    return drivers.map(driver => this.mapToEntity(driver));
   }
 
   async findOne(id: string): Promise<Driver | null> {
--- src/driver/entities/driver.entity.ts
+++ src/driver/entities/driver.entity.ts
@@ -8,4 +8,5 @@ export class Driver {
   isActive: boolean;
   email: string;
   password: string;
+  createdAt: Date;
 }
